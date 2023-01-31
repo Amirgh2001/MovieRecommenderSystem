bbc7de0f35240dab67a41eea058061d60ed01830
amir <amrghasemi1380@gmail.com>
2023-01-31 03:49:20 +0330

    fixing the Logic error, now project works well

--- webFront/script.js
+++ webFront/script.js
@@ -1,5 +1,5 @@
 // declare variables as a global variable
-let filmsMat, trainedModelMat=[], distances, given_film, sorted_distances, enumerated_distances = []
+let filmsMat, trainedModelMat=[]
 
 const films_csv_path = '../filmNames.csv'
 fetch(films_csv_path)
@@ -21,29 +21,31 @@ fetch(trainedModelMat_path)
 
 document.getElementById('start').addEventListener('click', (ev) => {
     ev.preventDefault()
-    given_film = document.getElementById('input').valueOf().value
+    let given_film = document.getElementById('input').valueOf().value
     document.getElementById('input').valueOf().value = ''
     submitForm(given_film)
 })
 
 const submitForm = (given_film) => {
+    let distances
     for (let i = 0; i < filmsMat.length; i++) {
         if (filmsMat[i] === given_film) {
             distances = trainedModelMat[i]
         }
     }
-    sorted_distances = enumerate(distances).sort((a, b) => b[1] - a[1])
-    console.log(sorted_distances)
+    distances = enumerate(distances).sort((a, b) => b[1] - a[1])
+    console.log(distances)
     let similar_movies = ''
     for (let i = 1; i < 10; i++) {
-        similar_movies = similar_movies + `<br>${i} - ` + filmsMat[sorted_distances[i][0]]
+        similar_movies = similar_movies + `<br>${i} - ` + filmsMat[distances[i][0]]
     }
     console.log(similar_movies)
     document.getElementById('response').style.display = "flex"
-    document.getElementById('response').innerHTML = `Similar movies with ${given_film} :` + similar_movies
+    document.getElementById('response').innerHTML = `Similar movies with ${given_film} :<br>` + similar_movies
 }
 
 const enumerate = (distances) => {
+    let enumerated_distances = []
     for (let i = 0; i < distances.length; i++) {
         enumerated_distances.push([i, distances[i]])
     }
